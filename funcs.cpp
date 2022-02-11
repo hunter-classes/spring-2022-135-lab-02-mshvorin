9276c90c295d5cfc79d5671b615b8ab887023dd8
mshvorin <matthew.shvorin21@sitechhs.com>
2022-02-10 20:54:02 -0500

    Fixed task B

--- funcs.cpp
+++ funcs.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
 void print_interval (int L,int U) {
-	std::cout << "Please Enter L: " << std::endl;
-	std::cin >> L;
-	std::cout << "Please Enter U: " << std::endl;
-	std::cin >> U;
 	do {
 	std::cout << L << " ";
 	L++;
